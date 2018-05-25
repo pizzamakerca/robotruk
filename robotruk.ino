d19907f2a6a4ad20b03d9536c89e49ff130c4a19
Andrew Edwards <pizzamakerca1@gmail.com>
2018-05-24 20:55:12 -0700

    Refactor code for move demo.

--- robotruk.ino
+++ robotruk.ino
@@ -11,6 +11,9 @@ int in3 = 7;
 int in4 = 6;
 int enB = 5;
 
+// set a global variable
+int stopper = 0;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -25,22 +28,32 @@ void setup() {
 }
 
 
-void demoThree() {
+void demoMove() {
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
   analogWrite(enA, 42);
   analogWrite(enB, 45);
-  delay(1000);
+  delay(3000);
   analogWrite(enA, 0);
   analogWrite(enB, 0);
   delay(2000);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  analogWrite(enA, 42);
+  analogWrite(enB, 45);
+  delay(3000);
+  analogWrite(enA, 0);
+  analogWrite(enB, 0);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  demoThree();
-  break;
-
+  if (stopper == 0) {
+    demoMove();
+    stopper = 1;
+  }
 }
